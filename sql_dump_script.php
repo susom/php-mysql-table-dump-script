f56f36dfda5abfbe7571da1fed176b198ba15e07
Andrew Martin <andy123@stanford.edu>
2022-11-12 20:36:25 -0800

    update gitignore

--- sql_dump_script.php
+++ sql_dump_script.php
@@ -2,109 +2,318 @@
 
 date_default_timezone_set('America/Los_Angeles');
 
-$config = parse_ini_file("config.ini");
-$username = $config['username'];
-$hostname = $config['hostname'];
-$password = $config['password'];
-$database = $config['database'];
-$increment_only = $config['increment_only'];
-$min_rows_for_increment = $config['min_rows_for_increment'];
 
-$destination_path = $config['destination_path'];
-$move_when_done_path = $config['move_when_done_path'];
+class rds
+{
+    public mysqli $conn;
 
+    private string $username;
+    private string $database;
+    private string $password;
+    private string $hostname;
 
-$log_file = $destination_path . "sql_dump.log";
-$cache_file = $destination_path . "chunked_tables.json";
-$pid_file = $destination_path . "pid_" . getmypid();
+    public int $increment_mode;
+    public int $increment_mode_min_rows;
+    public int $skip_dumps;
 
-file_put_contents($pid_file, date("Y-m-d H:i:s"));
+    public string $dump_working_path;
+    public string $dump_complete_path;
 
-$pid_segment_tables = ['redcap_data', 'redcap_metadata_archive'];
+    public string $increment_cache_file;
+    public string $log_file;
+    public string $pid_file;
 
-$inc_row_count = 0;
+    private array $range_tables;
+    public array $increment_cache;
 
-/**
- * @param $message
- * @return null
- */
-function logit($message, $log_file) {
-    $message = "[" . date("Y-m-d H:i:s") . "] " . $message;
-    echo $message;
-    file_put_contents($log_file, $message, FILE_APPEND);
-}
+    public function __construct()
+    {
+        $config = parse_ini_file("config.ini");
+        $this->username = $config['username'];
+        $this->hostname = $config['hostname'];
+        $this->password = $config['password'];
+        $this->database = $config['database'];
 
+        $this->increment_mode = $config['increment_mode'];
+        $this->increment_mode_min_rows = $config['increment_mode_min_rows'];
 
+        $this->dump_working_path = $config['dump_working_path'];
+        $this->dump_complete_path = $config['dump_complete_path'];
 
-if (file_exists($cache_file)) {
-    $cache = json_decode(file_get_contents($cache_file),true);
-} else {
-    // Initialize
-    $chunked_tables = [
-        'redcap_log_event' => 'log_event_id',
-        'redcap_log_event2' => 'log_event_id',
-        'redcap_log_event3' => 'log_event_id',
-        'redcap_log_event4' => 'log_event_id',
-        'redcap_log_event5' => 'log_event_id',
-        'redcap_log_event6' => 'log_event_id',
-        'redcap_log_event7' => 'log_event_id',
-        'redcap_log_event8' => 'log_event_id',
-        'redcap_log_event9' => 'log_event_id',
-        'redcap_log_view' => 'log_view_id',
-        'redcap_log_view_old' => 'log_view_id',
-        'redcap_outgoing_email_sms_log' => 'email_id',
-        'redcap_log_api_custom' => 'log_id'
-    ];
-    $cache = [];
-    foreach ($chunked_tables as $table=>$column) {
-        $cache[$table] = [
-            'column' => $column,
-            'last_max' => 0
-        ];
+        $this->skip_dumps = $config['skip_dumps'];
+
+        $this->log_file = $this->dump_working_path . "sql_dump.log";
+        $this->increment_cache_file = $this->dump_working_path . "increment_table_cache.json";
+        $this->pid_file = $this->dump_working_path . "pid_" . getmygid();
+
+        file_put_contents($this->pid_file, date("Y-m-d H:i:s"));
+
+        # Initiate database connection
+        if (! $this->conn = new mysqli($this->hostname, $this->username, $this->password, $this->database)) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
     }
-    file_put_contents("chunked_tables.json", json_encode($cache));
+
+
+    private function getRangeTables()
+    {
+        if(empty($this->range_tables)) {
+            $this->range_tables = json_decode(file_get_contents('default_range_tables.json'), true);
+        }
+        return $this->range_tables;
+    }
+
+    private function updateIncrementalLastMax($table, $last_max)
+    {
+        $cache = $this->getIncrementCache();
+        if (isset($cache[$table])) {
+            $cache[$table]['last_max'] = $last_max;
+        }
+        $this->saveIncrementCache($cache);
+    }
+
+    private function getIncrementCache()
+    {
+        if(empty($this->increment_cache)) {
+            if (!file_exists($this->increment_cache_file)) {
+                // Read from template
+                $this->increment_cache = json_decode(file_get_contents('default_increment_tables.json'), true);
+            } else {
+                $this->increment_cache = json_decode(file_get_contents($this->increment_cache_file), true);
+            }
+        }
+        return $this->increment_cache;
+    }
+
+    private function saveIncrementCache($cache)
+    {
+        file_put_contents($this->increment_cache_file, json_encode($cache));
+    }
+
+    private function logit($message)
+    {
+        $message = "[" . date("Y-m-d H:i:s") . "] " . $message;
+        echo $message . "\n";
+        file_put_contents($this->log_file, $message, FILE_APPEND);
+    }
+
+    private function getAllTables() {
+        $tables = [];
+        $sql = "show full tables where Table_type = 'BASE TABLE'";
+        $q = $this->conn->query($sql);
+        while ($row = $q->fetch_row()) {
+            $table = $row[0];
+            $tables[] = $table;
+        }
+        return $tables;
+    }
+
+
+    public function getDumps() {
+
+        $all_tables = $this->getAllTables();
+
+        $dumps = [];
+
+        # Process Incremental Tables
+        foreach ($this->getIncrementCache() as $table => $params) {
+            if ($this->increment_mode) {
+                $this->logit("Processing Increments");
+                if (in_array($table, $all_tables)) {
+                    unset($all_tables[$table]);
+                    $column = $params['column'];
+                    $last_max = $params['last_max'];
+                    $bin_count = $params['bin_count'];
+
+                    $ranges = $this->getBinValues($table, $column, $last_max, $bin_count, $this->increment_mode_min_rows);
+                    foreach ($ranges as $start => $end) {
+                        $dumps[] = [
+                            "table" => $table,
+                            "where" => "--where=\"$column > $start and $column <= $end\"",
+                            "create" => $start == 0 ? "" : "--skip-add-drop-table --no-create-info",
+                            "type" => "incremental",
+                            "last_max" => $end,
+                            "filename" => str_pad("i_" . $table, 25, "_", STR_PAD_RIGHT) .
+                                str_pad($start,10,"0",STR_PAD_LEFT) . "_" .
+                                str_pad($end,10,"0",STR_PAD_LEFT)
+
+                        ];
+                    }
+                }
+            }
+        }
+
+        # Process Range Tables
+        foreach ($this->getRangeTables() as $table => $params) {
+            if (in_array($table, $all_tables)) {
+                unset($all_tables[$table]);
+                $column = $params['column'];
+                $bin_count = $params['bin_count'];
+                $ranges = $this->getBinValues($table, $column, 0, $bin_count);
+
+                $digits = floor(log(count($ranges),10))+1;
+                $i = 0;
+                foreach ($ranges as $start => $end) {
+                    $i++;
+                    $part = str_pad($i,$digits,"0",STR_PAD_LEFT) . "of" . count($ranges);
+                    $dumps[] = [
+                        "table" => $table,
+                        "where" => "--where=\"$column > $start and $column <= $end\"",
+                        "create" => $start == 0 ? "" : "--skip-add-drop-table --no-create-info",
+                        "type" => "range",
+                        "last_max" => $end,
+                        "filename" => str_pad("r_" . $table, 25, "_",STR_PAD_RIGHT) .
+                            $part . "_" .
+                            str_pad($start,10,"0",STR_PAD_LEFT) . "_" .
+                            str_pad($end,10,"0",STR_PAD_LEFT)
+                    ];
+                }
+            }
+        }
+
+        # Process All Other Tables as one dump
+        $dumps[] = [
+            "table" => implode(" ", $all_tables),
+            "where" => "",
+            "create" => "",
+            "type" => "normal",
+            "filename" => "other_" . count($all_tables) . "_tables"
+        ];
+
+        return $dumps;
+    }
+
+    public function processDumps($dumps) {
+        foreach ($dumps as $dump) {
+            $table = $dump['table'];
+            $where = $dump['where'];
+            $create = $dump['create'];
+            $filename = $dump['filename'];
+            $command = "mysqldump $where --default-character-set=utf8mb4 --single-transaction " .
+                "--host=$this->hostname --user=$this->username --password=$this->password " .
+                "$create $this->database $table | gzip > $this->dump_working_path" . "$filename" . ".sql.gz";
+
+            $this->logit("Dump: $command");
+            if ($this->skip_dumps !== 1) {
+                exec($command);
+            }
+
+            if ($dump['type'] == "incremental") {
+                $this->updateIncrementalLastMax($table, $dump['last_max']);
+            }
+
+            // Move the completed file to a subdir
+//            exec("mv $destination_path$dest_name $move_when_done_path$dest_name");
+//            logit("Moved $dest_name to $move_when_done_path\n", $log_file);
+
+        }
+    }
+
+    private function getValueAtOffset($table, $column, $start, $offset) {
+        $sql = "select $column from $table where $column > $start order by $column limit 1 offset $offset";
+        $q = $this->conn->query($sql);
+        if ($row = $q->fetch_row()) {
+            $value = $row[0];
+        } else {
+            $value = false;
+        }
+        // $this->logit($sql . " [" . $value . "]");
+        return $value;
+    }
+
+    private function getMaxValue($table, $column) {
+        $sql = "select max($column) from $table";
+        $q = $this->conn->query($sql);
+        $row = $q->fetch_row();
+        return empty($row[0]) ? 0 : $row[0];
+    }
+
+
+    private function getCountValue($table, $where = "") {
+        $sql = "select count(*) from $table $where";
+        $q = $this->conn->query($sql);
+        $row = $q->fetch_row();
+        return $row[0];
+    }
+
+    private function getBinValues($table, $column, $start, $offset, $min_rows_last_bin = 0)
+    {
+        $max_value = $this->getMaxValue($table, $column);
+        $count = $this->getCountValue($table);
+        $values = [];
+        $this_value = $start;
+
+        $this->logit("$table has $count rows with max value of $max_value bin size of $offset");
+
+        while($next_value = $this->getValueAtOffset($table, $column, $this_value, $offset)) {
+            // $this->logit("$table starting at $this_value returned $next_value with offset $offset");
+            $values[$this_value] = $next_value;
+            $this_value = $next_value;
+        }
+
+        // Check to see if we should include the last bunch
+        if ($min_rows_last_bin > 0) {
+            // Get number of rows in the last range
+            $range_count = $this->getCountValue($table, " where $column > $this_value and $column <= $max_value");
+            if ($range_count > $min_rows_last_bin) {
+                $values[$this_value] = $max_value;
+            } else {
+                $this->logit("Skipping $range_count values over $this_value for $table because less than min_rows_last_bin $min_rows_last_bin");
+            }
+        } else {
+            // Always get the small remainder regardless of size
+            $values[$this_value] = $max_value;
+        }
+        return $values;
+    }
+
+
 }
 
 
+$rds = new rds();
 
+$dumps = $rds->getDumps();
 
-# Initiate database connection
-if (!$conn = new mysqli($hostname, $username, $password, $database)) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+$rds->processDumps($dumps);
 
+//var_dump($dumps);
+
+echo "Done";
+
+/*
 
 # Get All Tables and skip VIEWS
-$tables = [];
-$sql = "show full tables where Table_type = 'BASE TABLE'";
 
-$include_redcap_data = false;
 
-$q = $conn->query($sql);
-while ($row = $q->fetch_row()) {
-    $table = $row[0];
+# Build the dumps to be performed as part of this operation
+$dumps = [];
+foreach ($tables as $table) {
     if (in_array($table, $pid_segment_tables)) {
         // We want to break REDCap data and other pid-tables into multiple segments to speed up parallel uploading so
         // I'm going to hack in a where clause for the redcap_data queries...
-        $sql2 = "select max(project_id) from $table";
-        $q2 = $conn->query($sql2);
-        $row = $q2->fetch_row();
-        $max_pid = $row[0];
-        $pid_bin_size=500; // Break project exports by pid into bins of this range
-        $range_start=0;
-        $part=1;
-        while ($range_start <= $max_pid) {
-            $next_range_start = $range_start + $pid_bin_size;
-            $tables[] = $table . "|" .
-                str_pad($part,3,"0",STR_PAD_LEFT) . "|" .
-                "project_id >= $range_start and project_id < $next_range_start";
-            $range_start = $next_range_start;
-            $part++;
-        }
-    } else {
-        $tables[] = $table;
-    }
+
+
 }
+
+    $sql2 = "select max(project_id) from $table";
+    $q2 = $conn->query($sql2);
+    $row = $q2->fetch_row();
+    $max_pid = $row[0];
+    $pid_bin_size=500; // Break project exports by pid into bins of this range
+    $range_start=0;
+    $part=1;
+    while ($range_start <= $max_pid) {
+        $next_range_start = $range_start + $pid_bin_size;
+        $tables[] = $table . "|" .
+            str_pad($part,3,"0",STR_PAD_LEFT) . "|" .
+            "project_id >= $range_start and project_id < $next_range_start";
+        $range_start = $next_range_start;
+        $part++;
+    }
+} else {
+
+
 
 //var_dump($tables);
 
@@ -175,7 +384,7 @@ foreach ($tables as $table) {
     }
 
     exec("mysqldump $where --default-character-set=utf8mb4 --single-transaction --host=$hostname --user=$username --password=$password $skipCreate $database $table | gzip > $destination_path$dest_name");
-    file_put_contents($cache_file, json_encode($cache));
+    file_put_contents($increment_cache_file, json_encode($cache));
 
     // Move the completed file to a subdir
     exec("mv $destination_path$dest_name $move_when_done_path$dest_name");
@@ -187,3 +396,5 @@ unlink($pid_file);
 
 logit("----DONE---- ($inc_row_count incremental rows)\n", $log_file);
 echo "\n";
+
+*/
