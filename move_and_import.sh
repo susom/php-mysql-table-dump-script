17524be4ca03d9b708ca64198171dead4c16a886
Andrew Martin <andy123@stanford.edu>
2023-07-30 12:12:11 -0700

    try 2 functions

--- move_and_import.sh
+++ move_and_import.sh
@@ -10,7 +10,43 @@ source move_and_import_config.sh
 echo "----------------------------"
 echo $(date -u) STARTING
 
-i=0
+
+function waitForProcess() {
+  RUNNING_PROCESS_NAME=$(gcloud sql operations list --instance=${INSTANCE} | grep "RUNNING" | cut -d' ' -f1)
+  if [[ -z "$RUNNING_PROCESS_NAME" ]]; then
+    # Nothing running -- go ahead
+    return 1
+  else
+    gcloud sql operations wait "$PROCESS_NAME" --timeout=7200 --verbosity="critical" 2>&1 | tee -a $OUTPUTFILE
+    return waitForProcess
+  fi
+}
+
+
+function importBucket() {
+  echo "$(date -u) [$filename] Starting Import into $INSTANCE $DESTDB" | tee -a $OUTPUTFILE
+  RESULT=$(gcloud sql import sql $INSTANCE "gs://$BUCKET/$BUCKETFOLDER/$filename" --database=$DESTDB --quiet 2>&1)
+  echo "$RESULT" | tee -a $OUTPUTFILE
+  if [[ $RESULT == "Imported data"* ]]; then
+    #success
+    return 1
+  elif [[ $RESULT == *"longer than expected"* ]]; then
+    # hasn't finished
+    waitForProcess
+    # assume we end with success
+  elif [[ $RESULT == *"in progress"* ]]; then
+    echo "$(date -u) [$filename] Another operation in progress..." | tee -a $OUTPUTFILE
+    waitForProcess
+    importBucket
+  elif [[ $RESULT == *"ERROR"* ]]; then
+    echo "Unknown error" | tee -a $OUTPUTFILE
+    return 0;
+  else
+    echo "Unknown response - $RESULT" | tee -a $OUTPUTFILE
+    return 0;
+  fi
+}
+
 while true
 do
   cnt=$(ls -1q | grep .sql.gz$ | wc -l)
@@ -18,51 +54,32 @@ do
   if [ $cnt -eq 0 ]; then
     # no files to process - so sleep for a minute and check again
     echo "."
-    sleep 60
+    sleep 30
   else
-    for filename in $(ls | grep .sql.gz$) ; do
-      ((i=i+1))
-      echo "$(date -u) [$i] $filename: copying to $BUCKET/$BUCKETFOLDER" | tee -a $OUTPUTFILE
-      gsutil cp $filename gs://$BUCKET/$BUCKETFOLDER/$filename
+    filename=$(ls | grep .sql.gz$ | head -n 1)
+
+    #step 1: copy file to bucket
+    echo "$(date -u) [$filename] (1 of $cnt) Copying to $BUCKET/$BUCKETFOLDER" | tee -a $OUTPUTFILE
+    gsutil cp $filename gs://$BUCKET/$BUCKETFOLDER/$filename
+
+    #step 2: try import it
+    # check that no active processes are running before beginning import
+    waitForProcess
+    IMPORT_RESULT=importBucket
+
+    if [[ $IMPORT_RESULT ]]; then
+      #success
       # rename local file by appending a done suffix
-      rm -f "$filename".done 2> /dev/null
-      mv "$filename" "done/$filename"
       # start importing the file
-      # check that no active processes are running before beginning import
-      RUNNING_PROCESS_NAME=$(gcloud sql operations list --instance=${INSTANCE} | grep "RUNNING" | cut -d' ' -f1)
-      until [[ -z "$RUNNING_PROCESS_NAME" ]]; do
-         # we have a running process -- we must wait for it
-         echo "$(date -u) [$i] $filename: process $RUNNING_PROCESS_NAME still running - waiting to complete..." | tee -a $OUTPUTFILE
-         gcloud sql operations wait "$RUNNING_PROCESS_NAME" --timeout=1800 --verbosity="critical" 2>&1 | tee -a $OUTPUTFILE
-         RUNNING_PROCESS_NAME=$(gcloud sql operations list --instance=${INSTANCE} | grep "RUNNING" | cut -d' ' -f1)
-      done
-      echo "$(date -u) [$i] $filename: Starting Import into $INSTANCE $DESTDB" | tee -a $OUTPUTFILE
-      while true; do
-        RESULT=$(gcloud sql import sql $INSTANCE "gs://$BUCKET/$BUCKETFOLDER/$filename" --database=$DESTDB --quiet 2>&1)
-        echo "$RESULT" | tee -a $OUTPUTFILE
-        if [[ $RESULT == *"longer than expected"* ]]; then
-            echo "$(date -u) [$i] $filename: Timeout" | tee -a $OUTPUTFILE
-	    RUNNING_PROCESS_NAME=$(gcloud sql operations list --instance=${INSTANCE} | grep "RUNNING" | cut -d' ' -f1)
-	    echo "$(date -u) [$i] $filename: Import process $RUNNING_PROCESS_NAME still running - waiting to complete..." | tee -a $OUTPUTFILE
-            gcloud sql operations wait "$RUNNING_PROCESS_NAME" --timeout=1800 --verbosity="critical" 2>&1 | tee -a $OUTPUTFILE	
-            sleep 10
-          elif [[ $RESULT == *"in progress"* ]]; then
-            echo "$(date -u) [$i] $filename: Another operation in progress - waiting 10 sec to try again" | tee -a $OUTPUTFILE
-            sleep 10
-          elif [[ $RESULT == *"ERROR"* ]]; then
-            echo "Unknown error" | tee -a $OUTPUTFILE
-            sleep 60
-            break;
-          else
-            # assume success
-            break;
-        fi
-      done
-      echo "---------------------------------" | tee -a $OUTPUTFILE
-      # take a breath
-      sleep 2
-    done
-    echo "Finished loop $i processing $cnt files" | tee -a $OUTPUTFILE
+      echo "Finished $filename moving to done" | tee -a $OUTPUTFILE
+      mv "$filename" "done/$filename"
+    el
+      #error
+      echo "Finished $filename with error" | tee -a $OUTPUTFILE
+      mv "$filename" "error/$filename"
+    fi
+    # take a breath
+    sleep 1
   fi
 done
 
